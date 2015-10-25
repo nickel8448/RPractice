53274faa7fbc5538aca81e6c7779e42c75d4c7f5
rahulw <rahulwadhwani21@gmail.com>
2015-10-25 23:36:21 +0530

    Changed spacing in the code

--- Jagriti Yatra/states.R
+++ Jagriti Yatra/states.R
@@ -8,7 +8,10 @@ ggplot(location, aes(x=Location, y=freq, ymax=max(location$freq))) +
   geom_bar(stat="identity", width=0.8, fill="#e67e22") + 
   xlab("States") + 
   ggtitle("States graph for Jagriti Yatra") +  # Title of the graph
-  theme(axis.text.x = element_text(angle = 45, hjust = 1), panel.grid.minor = element_blank(), panel.border=element_blank(), panel.background=element_blank()) + # Tilting the x label and modifying the background
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), 
+  panel.grid.minor = element_blank(), 
+  panel.border=element_blank(), 
+  panel.background=element_blank()) + # Tilting the x label and modifying the background
   geom_text(aes(label=freq), position=position_dodge(width=0.9), vjust=-0.40, size=3) +
   scale_y_continuous(breaks = round(seq(0, max(location$freq), by=20)), "Number of People") +  # Chanding the scale
   ggsave("plot.png", dpi=300)
