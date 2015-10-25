93d8a80635d05657c13cc85c5be158ca6376c29b
rahulw <rahulwadhwani21@gmail.com>
2015-10-25 23:36:42 +0530

    Added another graph

--- Newspaper Ads Analysis/Analysis.R
+++ Newspaper Ads Analysis/Analysis.R
@@ -35,3 +35,39 @@ for (i in 1:length(companies2)){
   }
 }
 
+allCompanies <- data.frame(allCompanies)  # Converting to data frame
+allCompanies <- count(allCompanies, "allCompanies")  # Counting the number of companies
+
+# Making a new variable Bar Graph
+cname <- vector(mode = "character", length=0)
+cvalue <- vector(mode = "integer", length=0)
+for (i in 1:nrow(allCompanies))
+{
+  if (allCompanies[i, 2] <= 1)
+  {
+    if (!('Others' %in% cname))
+    {
+      cname <- c(cname, 'Others')
+      cvalue[match('Others', cname)] = 1
+    }
+    else
+    {
+      cvalue[match('Others', cname)] = I(cvalue[match('Others', cname)] + 1)
+    }
+  }
+  else
+  {
+    cname <- c(cname, as.character(allCompanies[i, 1]))  # To get the company name
+    cvalue[match(allCompanies[i, 1], cname)] = allCompanies[i, 2]  # To get the value of its occurance
+  }
+}
+
+CompaniesCount <- data.frame(cname, cvalue)
+names(CompaniesCount) <- c("CompanyName", "Occurrence")
+
+ggplot(data = CompaniesCount, aes(x = CompanyName, y = Occurrence)) + 
+  scale_y_continuous(breaks = round(seq(0, max(15), by=2))) + 
+  theme(panel.border=element_blank(), 
+        panel.background=element_blank()) + 
+  geom_bar(stat = "identity")
+
