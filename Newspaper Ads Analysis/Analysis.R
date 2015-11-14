d33fc252b1d798c33e725917269a46979b4672ec
rahulw <rahulwadhwani21@gmail.com>
2015-11-14 19:17:27 +0530

    Added new functions and made new graphs

--- Newspaper Ads Analysis/Analysis.R
+++ Newspaper Ads Analysis/Analysis.R
@@ -4,70 +4,155 @@ library("stringr")  # Library to strip the string
 library("plyr")
 
 ns <- read.csv("NS.csv")  # Reading the CSV
-
 # Computing Days with Number of News Pages
 
-newsPages <- data.frame(ns$Date, ns$Full.Page.Ads, I(ns$Total.Pages - ns$Full.Page.Ads))
-newsHeading <- c("Date", "FullPageAds", "NewsPages")  # Renaming the columns
-names(newsPages) <- newsHeading
-newsPages2 <- melt(newsPages)
-ggplot(data = newsPages2, aes(x = Date, y = value, fill = variable)) +
-  geom_bar(stat = "identity") +
-  ylab("Number of Pages") +
-  scale_fill_manual(values = c("#E24139", "#158504"),
-                    name = "Pages Composition",
-                    breaks = c("FullPageAds", "NewsPages"),
-                    labels = c("Full Page Ads", "Pages with News")) +
-  theme(panel.background = element_blank()) + # No background
-  coord_flip()
-
-# Companies mostly taking the front page
-
-companies <- ns$Front.Page.Ad.By  # Getting all the companies
-companies2 <- strsplit(as.character(companies), ",", fixed=TRUE)  # Splitting the Companies
-
-allCompanies <- vector(mode="numeric", 0)  # Empty Vector to store companies
-
-# Loop to get all the companies indivually 
-for (i in 1:length(companies2)){
-  for (o in 1:length(companies2[[i]])){
-    allCompanies <- c(allCompanies, str_trim(companies2[[i]][o]))
-  }
+themefunc <- function()
+{
+  ##Function for the theming of all the graphs
+  themev <- theme(panel.background = element_blank(),
+          axis.text.x = element_text(size = 11, colour="black"),
+          axis.text.y = element_text(size = 11, colour="black"),
+          axis.title.y = element_text(size = 14, colour="black", vjust = 1.5),
+          axis.title.x = element_text(size = 14, colour="black", vjust=-.5)) # No background
+  return(themev)
 }
 
-allCompanies <- data.frame(allCompanies)  # Converting to data frame
-allCompanies <- count(allCompanies, "allCompanies")  # Counting the number of companies
+# Make a new data frame
+newsPages <- data.frame(ns$Date, ns$Full.Page.Ads, I(ns$Total.Pages - ns$Full.Page.Ads))
+# Rename the Columns
+names(newsPages) <- c("Date", "FullPageAds", "NewsPages")
+# Melt the Date
+newsPages2 <- melt(newsPages)
+# Reorder it based on the date
+newsPages2 <- transform(newsPages2, Date = reorder(Date, order(as.Date(newsPages2$Date, format="%d/%m/%Y"), decreasing = TRUE)))
+# Making a new variable label_y to know the point where label should be
+newsPages2 <- ddply(newsPages2, "Date", mutate, label_y = cumsum(value) - .5*value)
+# Making the plot
+ggplot(data = newsPages2, aes(x = Date, y = value, fill = variable)) +
+  geom_bar(stat = "identity") +
+  ylab("Total Number of Pages") +  # putting the label for y axis
+  scale_fill_manual(values = c("#E24139", "#158504"),  # Chaning the colour of the bars
+                    name = "Pages Composition",  # Name of the Graph
+                    breaks = c("FullPageAds", "NewsPages"),  # Name of the legend
+                    labels = c("Full Page Ads", "Pages with News")) +  # Changing the legend
+  geom_text(aes(y=label_y, label=value), vjust=0.5, color='white', fontface=1, size=3) +  # Putting the labels
+  themefunc() +
+  coord_flip()  # Flipping the graph
 
-# Making a new variable Bar Graph
-cname <- vector(mode = "character", length=0)
-cvalue <- vector(mode = "integer", length=0)
-for (i in 1:nrow(allCompanies))
+#######
+
+# Weeks and Numbers of ads for every week day
+
+AllDayAds <- data.frame(ns$Full.Page.Ads, ns$Day, ns$Total.Pages)
+days <- vector(mode = "character", length=0)
+daysFreq <- vector(mode = "integer", length=0)
+totalPages <- vector(mode = "integer", length=0)
+for (i in 1:nrow(AllDayAds))
 {
-  if (allCompanies[i, 2] <= 1)
+  if(as.character(AllDayAds[i, 2]) %in% as.character(days))  # Checking the day is already present in the vector days
   {
-    if (!('Others' %in% cname))
-    {
-      cname <- c(cname, 'Others')
-      cvalue[match('Others', cname)] = 1
-    }
-    else
-    {
-      cvalue[match('Others', cname)] = I(cvalue[match('Others', cname)] + 1)
-    }
+    daysFreq[match(as.character(AllDayAds[i, 2]), days)] = I(daysFreq[match(as.character(AllDayAds[i, 2]), days)] + AllDayAds[i, 1])
+    totalPages[match(as.character(AllDayAds[i, 2]), days)] = I(totalPages[match(as.character(AllDayAds[i, 2]), days)] + AllDayAds[i, 3])
   }
   else
   {
-    cname <- c(cname, as.character(allCompanies[i, 1]))  # To get the company name
-    cvalue[match(allCompanies[i, 1], cname)] = allCompanies[i, 2]  # To get the value of its occurance
+    days <- c(days, as.character(AllDayAds[i, 2]))
+    daysFreq <- c(daysFreq, AllDayAds[i, 1])
+    totalPages <- c(totalPages, AllDayAds[i, 3])
   }
 }
 
-CompaniesCount <- data.frame(cname, cvalue)
-names(CompaniesCount) <- c("CompanyName", "Occurrence")
+# Making a new data frame for the Days and Number of ads
+AllDayAds <- data.frame(days, daysFreq, totalPages)
+names(AllDayAds) <- c("WDay", "Pages", "TotalPages")
+# Increasing the number of pages for Friday as there was a holiday and averaging
+for (i in 1:nrow(AllDayAds))
+{
+  if (as.character(AllDayAds[i, 1]) == "Friday")
+  {
+    AllDayAds[i, 2] = AllDayAds[i, 2] + (AllDayAds[i, 2]/3)
+    AllDayAds[i, 3] = AllDayAds[i, 3] + (AllDayAds[i, 3]/3)
+  }
+  AllDayAds[i, 2] = AllDayAds[i, 2] / 4
+  AllDayAds[i, 3] = AllDayAds[i, 3] / 4
+}
 
-ggplot(data = CompaniesCount, aes(x = CompanyName, y = Occurrence)) + 
-  scale_y_continuous(breaks = round(seq(0, max(15), by=2))) + 
-  theme(panel.border=element_blank(), 
-        panel.background=element_blank()) + 
-  geom_bar(stat = "identity")
+meltedAllDayAds <- melt(AllDayAds)
+meltedAllDayAds <- ddply(meltedAllDayAds, "WDay", mutate, label_y = cumsum(value) - .5*value)
+ggplot(meltedAllDayAds, aes(x = WDay, y = value, fill = variable)) +
+  geom_bar(stat = "identity") +
+  ylab("Total Pages") +
+  xlab("Day of the Week") +
+  scale_fill_manual(values = c("#E24139", "#158504"),  # Chaning the colour of the bars
+                    name = "Pages Composition",
+                    breaks = c("Pages", "TotalPages"),
+                    labels = c("Full Page Ads", "Pages with News")) + 
+  geom_text(aes(y=label_y, label=value), vjust=0.5, color='white', fontface=1, size=5) +  # Putting the labels
+  themefunc() 
 
+######
+# Companies mostly taking the front page
+
+companiesFunction <- function(Variable)
+{
+  companies <- ns[Variable]  # Getting all the companies
+  companies2 <- vector(mode = "character", length = 0)
+  for (i in 1:nrow(companies))
+  {
+    print (companies[i, 1])
+    companies2 <- c(companies2, strsplit(as.character(companies[i, 1]), ",", fixed = TRUE))
+  }
+  allCompanies <- vector(mode="numeric", 0)  # Empty Vector to store companies
+
+  # Loop to get all the companies indivually 
+  for (i in 1:length(companies2)){
+    for (o in 1:length(companies2[[i]])){
+      allCompanies <- c(allCompanies, str_trim(companies2[[i]][o]))
+    }
+  }
+
+  allCompanies <- data.frame(allCompanies)  # Converting to data frame
+  allCompanies <- count(allCompanies, "allCompanies")  # Counting the number of companies
+
+  # Making a new variable Bar Graph
+  cname <- vector(mode = "character", length=0)
+  cvalue <- vector(mode = "integer", length=0)
+  for (i in 1:nrow(allCompanies))
+  {
+    if (allCompanies[i, 2] <= 1)
+    {
+      if (!('Others' %in% cname))
+      {
+        cname <- c(cname, 'Others')
+        cvalue[match('Others', cname)] = 1
+      }
+      else
+      {
+        cvalue[match('Others', cname)] = I(cvalue[match('Others', cname)] + 1)
+      }
+    }
+    else
+    {
+      cname <- c(cname, as.character(allCompanies[i, 1]))  # To put the company name
+      cvalue[match(allCompanies[i, 1], cname)] = allCompanies[i, 2]  # To get the value of its occurance
+    }
+  }
+
+  CompaniesCount <- data.frame(cname, cvalue)
+  names(CompaniesCount) <- c("CompanyName", "Occurrence")
+  levels(CompaniesCount$CompanyName) <- gsub(" ", "\n", levels(CompaniesCount$CompanyName))  # To put a line break in the company names
+  return (CompaniesCount)
+  
+}
+
+FrontPageComp <- companiesFunction("Front.Page.Ad.By")
+FullPageComp <- companiesFunction("Full.Page.Ads.By")
+
+ggplot(data = CompaniesCount, aes(x = reorder(CompanyName, Occurrence), y = Occurrence, ymax=max(Occurrence))) + 
+    scale_y_continuous(breaks = round(seq(0, max(30), by=2))) + 
+    themefunc() + 
+    xlab("Companies") +
+    ylab("Number of times Ad Occurred") +
+    ggtitle("Frequency of various Companies occurring on 1st Page") +
+    geom_text(aes(label=Occurrence), position=position_dodge(width=0.9), vjust=-0.4, size=4) +
+    geom_bar(stat = "identity", width=0.5, position = position_dodge(width=1), fill="#66bb6a")
