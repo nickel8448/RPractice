421873974859803779aae3ec20a0fc0e0525018a
rahulw <rahulwadhwani21@gmail.com>
2015-11-26 09:53:58 +0530

    Changed a graph

--- Newspaper Ads Analysis/Analysis.R
+++ Newspaper Ads Analysis/Analysis.R
@@ -11,10 +11,10 @@ themefunc <- function()
 {
   ##Function for the theming of all the graphs
   themev <- theme(panel.background = element_blank(),
-          axis.text.x = element_text(size = 11, colour="black"),
-          axis.text.y = element_text(size = 11, colour="black"),
-          axis.title.y = element_text(size = 14, colour="black", vjust = 1.5),
-          axis.title.x = element_text(size = 14, colour="black", vjust=-.5)) # No background
+          axis.text.x = element_text(size = 11.5, colour="black"),
+          axis.text.y = element_text(size = 11.5, colour="black"),
+          axis.title.y = element_text(size = 14.5, colour="black", vjust = 1.5),
+          axis.title.x = element_text(size = 14.5, colour="black", vjust=-.5)) # No background
   return(themev)
 }
 
@@ -33,13 +33,15 @@ newsPages2 <- ddply(newsPages2, "Date", mutate, label_y = cumsum(value) - .5*val
 ggplot(data = newsPages2, aes(x = Date, y = value, fill = variable)) +
   geom_bar(stat = "identity") +
   ylab("Total Number of Pages") +  # putting the label for y axis
-  scale_fill_manual(values = c("#E24139", "#158504"),  # Chaning the colour of the bars
+  scale_fill_manual(values = c("#424242", "#2E7D32"),  # Chaning the colour of the bars
                     name = "Pages Composition",  # Name of the Graph
                     breaks = c("FullPageAds", "NewsPages"),  # Name of the legend
                     labels = c("Full Page Ads", "Pages with News")) +  # Changing the legend
-  scale_y_continuous(breaks = round(seq(0, max(30), by=5))) +
+  scale_y_continuous(breaks = round(seq(0, max(30), by=3))) +
   geom_text(aes(y=label_y, label=value), vjust=0.5, color='white', fontface=1, size=3) +  # Putting the labels
   themefunc() +
+  ggtitle("Composition of Pages in the Newspaper") +
+  theme(plot.title = element_text(lineheight=.8, face="bold")) +
   coord_flip() +  # Flipping the graph
   ggsave("Ads.png", dpi=300) 
 
@@ -87,16 +89,19 @@ for (i in 1:nrow(AllDayAds))
 
 meltedAllDayAds <- melt(AllDayAds)
 meltedAllDayAds <- ddply(meltedAllDayAds, "WDay", mutate, label_y = cumsum(value) - .5*value)
+meltedAllDayAds$WDay <- factor(meltedAllDayAds$WDay, levels= c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"))
+meltedAllDayAds[order(meltedAllDayAds$WDay), ]
 ggplot(meltedAllDayAds, aes(x = WDay, y = value, fill = variable)) +
-  geom_bar(stat = "identity") +
+  geom_bar(stat = "identity", width = 0.75) +
   ylab("Total Pages") +
   xlab("Day of the Week") +
-  scale_fill_manual(values = c("#E24139", "#158504"),  # Changing the colour of the bars
+  scale_fill_manual(values = c("#424242", "#2E7D32"),  # Changing the colour of the bars
                     name = "Pages Composition",
                     breaks = c("Pages", "TotalPages"),
                     labels = c("Full Page Ads", "Pages with News")) + 
   geom_text(aes(y=label_y, label=value), vjust=0.5, color='white', fontface=1, size=5) +  # Putting the labels
   themefunc() +
+  guides(fill = guide_legend(reverse=TRUE)) +  # To reverse the order of the legend
   ggsave('Days.png', dpi=300)
 
 #####
@@ -122,26 +127,14 @@ companiesFunction <- function(Variable)
   }
 
   allCompanies <- data.frame(allCompanies)  # Converting to data frame
-  allCompanies <- count(allCompanies, "allCompanies")  # Counting the number of companies
-
+  allCompanies <- plyr::count(allCompanies, "allCompanies")  # Counting the number of companies
+  
   # Making a new variable Bar Graph
   cname <- vector(mode = "character", length=0)
   cvalue <- vector(mode = "integer", length=0)
   for (i in 1:nrow(allCompanies))
   {
-    if (allCompanies[i, 2] <= 1)
-    {
-      if (!('Others' %in% cname))
-      {
-        cname <- c(cname, 'Others')
-        cvalue[match('Others', cname)] = 1
-      }
-      else
-      {
-        cvalue[match('Others', cname)] = I(cvalue[match('Others', cname)] + 1)
-      }
-    }
-    else
+    if (allCompanies[i, 2] >= 2)
     {
       cname <- c(cname, as.character(allCompanies[i, 1]))  # To put the company name
       cvalue[match(allCompanies[i, 1], cname)] = allCompanies[i, 2]  # To get the value of its occurance
@@ -152,8 +145,8 @@ companiesFunction <- function(Variable)
   names(CompaniesCount) <- c("CompanyName", "Occurrence")  # Putting a name to the data frame
    # To put a line break in the company names
   return (CompaniesCount)
-  
 }
+
 
 FrontPageComp <- companiesFunction("Front.Page.Ad.By")
 FullPageComp <- companiesFunction("Full.Page.Ads.By")
@@ -168,20 +161,20 @@ ggplot(data = FrontPageComp, aes(x = reorder(CompanyName, Occurrence), y = Occur
     xlab("Companies") +
     ylab("Number of times Ad Occurred") +
     ggtitle("Frequency of various Companies occurring on 1st Page") +
-    geom_text(aes(label=Occurrence), position=position_dodge(width=0.9), vjust=-0.4, size=4) +
-    geom_bar(stat = "identity", width=0.5, position = position_dodge(width=1), fill="#158504") +
+    geom_text(aes(label=Occurrence), position=position_dodge(width=0.9), vjust=-0.4, size=5) +
+    geom_bar(stat = "identity", position = position_dodge(width = 0.1), width=0.69, fill="#2E7D32") +
   ggsave("FrontPage.png", dpi=300)
 
 
 # Plotting for Companies which can give Full Page Ads
 ggplot(data = FullPageComp, aes(x = reorder(CompanyName, -Occurrence), y = Occurrence, ymax=max(Occurrence))) + 
-    scale_y_continuous(breaks = round(seq(0, max(50), by=2))) + 
+    scale_y_continuous(breaks = round(seq(0, max(50), by=3))) + 
     themefunc() + 
     xlab("Companies") +
     ylab("Number of times Ad Occurred") +
     ggtitle("Frequency of various Companies Giving Full Page Ads") +
     geom_text(aes(label=Occurrence), position=position_dodge(width=0.9), vjust=0.5, size=4, hjust=-0.5) +
-    geom_bar(stat = "identity", width=0.5, position = position_dodge(0.7), fill="#158504") +
+    geom_bar(stat = "identity", width=0.6, position = position_dodge(0.2), fill="#2E7D32") +
     coord_flip() +
     ggsave("FullPage.png", dpi=300)
 
